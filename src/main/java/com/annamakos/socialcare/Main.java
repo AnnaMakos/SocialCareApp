9c37d8cd7ac8c81eff8f40c6029a25ce5456acb3
AnnaMakos <anna.makos@student.wat.edu.pl>
2019-10-29 18:09:03 +0100

    springboot project created

---
+++ src/main/java/com/annamakos/socialcare/Main.java
@@ -0,0 +1,21 @@
+package com.annamakos.socialcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class Main {
+
+    @RequestMapping(value="/xd", method= RequestMethod.GET)
+    public String welcome(){
+        return "gowno";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
